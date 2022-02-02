c834a7a8a7ed924faee0263d5d1842dcb4a6bade
Shwetha <Shwetha@DESKTOP-TLC82K7>
2022-02-02 16:28:08 +0530

    second commit

---
+++ C2CT-B10/src/JavaBasics/VowelOrConsonant.java
@@ -0,0 +1,18 @@
+package JavaBasics;
+
+public class VowelOrConsonant 
+{
+	public static void main(String[] args) 
+	{
+		char s= 's';
+	    if(s=='A' | s=='E' | s=='I' | s=='O' | s=='U' | s=='a' | s=='e' | s=='i' | s=='o' | s=='u')
+	    {
+	    	System.out.println(s+" is a vowel");
+	    }
+	    else
+	    {
+	    	System.out.println(s+ " is a consonant");
+	    }
+	}
+}
+
