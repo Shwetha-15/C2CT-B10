f6ddccb2cb6c4b767588203f10505392a6d496c2
Shwetha <shwetha151998@gmail.com>
2022-02-05 13:37:45 +0530

    8th commit

---
+++ C2CT-B10/src/assignment_1/Multiplication.java
@@ -0,0 +1,14 @@
+//WAP to generate multiplication table
+package assignment_1;
+
+public class Multiplication 
+{
+    public static void main(String[] args) 
+    {
+		int num=5;
+		for(int i=1;i<=10;i++)
+		{
+			System.out.printf("%d * %d = %d \n ",num,i,num*i);
+		}
+	}
+}
