20ad310d9a504bcc2b016b67bfcf0c71ad3ddaf9
Shwetha-15 <shwetha151998@gmail.com>
2022-02-05 21:29:46 +0530

    9th commit

---
+++ C2CT-B10/src/assignment_1/LeapYear.java
@@ -0,0 +1,17 @@
+package assignment_1;
+
+public class LeapYear 
+{
+    public static void main(String[] args) 
+    {
+		int year=2016;
+		if((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0))
+		{
+			System.out.println("leap year");
+		}
+		else
+		{
+			System.out.println("not a leap year");
+		}
+	}
+}
