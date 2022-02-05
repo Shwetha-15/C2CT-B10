8f031e6e6efcd4c994219a5367f2f0e6605b7490
Shwetha <shwetha151998@gmail.com>
2022-02-05 14:08:36 +0530

    1st commit

---
+++ C2CT-B10/src/JavaBasics/Program1.java
@@ -0,0 +1,8 @@
+package JavaBasics;
+public class Program1 
+{
+    public static void main(String[] args) 
+    {
+		System.out.println("hello world");
+	}
+}
---
+++ C2CT-B10/src/assignment_1/Program_1.java
@@ -0,0 +1,9 @@
+//Hello world program
+package assignment_1;
+public class Program_1 
+{
+    public static void main(String[] args) 
+    {
+	    System.out.println("hello world");	
+	}
+}
