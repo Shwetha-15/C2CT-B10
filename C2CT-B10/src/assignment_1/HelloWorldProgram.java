d0460bc5213d0327165ca3daa56d325c6f5ae6c1
Shwetha <shwetha151998@gmail.com>
2022-02-05 13:53:29 +0530

    1st commit

---
+++ C2CT-B10/src/assignment_1/HelloWorldProgram.java
@@ -0,0 +1,9 @@
+//Hello world program
+package assignment_1;
+public class HelloWorldProgram 
+{
+    public static void main(String[] args) 
+    {
+	     System.out.println("This is my first hello world program");	
+	}
+}
