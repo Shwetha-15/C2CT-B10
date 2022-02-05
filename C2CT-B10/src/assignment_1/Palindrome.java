5869e09d79d762cab702edb90335ef613c4ea4f4
Shwetha <shwetha151998@gmail.com>
2022-02-05 12:38:24 +0530

    5th commit

--- C2CT-B10/src/assignment_1/Palindrome.java
+++ C2CT-B10/src/assignment_1/Palindrome.java
@@ -4,26 +4,22 @@ public class Palindrome
 {
 	public static void main(String[] args) 
 	{
-         int num=515;
-         if(isPalindrome(num))
+        int num=515;
+        int rem,rev=0;
+     	int temp=num;
+     	while(num!=0)
+     	{
+     		rem=num%10;
+     		rev=rev*10+rem;
+     		num=num/10;
+     	}
+         if(temp==rev)
          {
-        	 System.out.println(num +" is a Palindrome");
+        	 System.out.println(temp +" is a Palindrome");
          }
          else
          {
-        	 System.out.println(num +" is not a Palindrome");
+        	 System.out.println(temp +" is not a Palindrome");
          }
 	}
-    public static boolean isPalindrome(int num)
-    {
-    	int rem,rev=0;
-    	int temp=num;
-    	while(num>0)
-    	{
-    		rem=num%10;
-    		rev=rev*10+rem;
-    		num=num/10;
-    	}
-    	return rev==temp;
-    }
 }
