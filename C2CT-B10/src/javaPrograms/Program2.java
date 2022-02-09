efd2e514ba0b9fe0d042e994d019e197a929f3d8
Shwetha-15 <shwetha151998@gmail.com>
2022-02-09 22:29:59 +0530

    1st commit

---
+++ C2CT-B10/src/javaPrograms/Program2.java
@@ -0,0 +1,29 @@
+package javaPrograms;
+
+public class Program2 
+{
+    	void addition(int a,int b,int c)
+    	{
+    		System.out.println("result = "+(a+b+c));
+    	}
+    	void addition(int a,int b,double c)
+    	{
+    		System.out.println("result = "+(a+b+c));
+    	}
+    	void addition(int a,int b,int c,int d)
+    	{
+    		System.out.println("result = "+(a+b+c+d));
+    	}
+    	void addition(int a,int b)
+    	{
+    		System.out.println("result = "+(a+b));
+    	}
+    public static void main(String[] args)
+    {
+    	Program2 o=new Program2();
+    	o.addition(2, 5);//int,int
+    	o.addition(2, 4,1,5);//int,int,int,int
+    	o.addition(1,2,3);//int,int,int
+    	o.addition(2,8, 5.6);//int,int,double
+	}
+}
