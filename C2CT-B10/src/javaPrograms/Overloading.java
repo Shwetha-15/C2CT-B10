0305f6ec7d63098700ca822e1082e7385f66d027
Shwetha-15 <shwetha151998@gmail.com>
2022-02-10 14:37:54 +0530

    2nd commit

---
+++ C2CT-B10/src/javaPrograms/Overloading.java
@@ -0,0 +1,29 @@
+//Inheritance
+package javaPrograms;
+public class Overloading 
+{
+	void addition(int a,int b,int c)
+	{
+		System.out.println("result = "+(a+b+c));
+	}
+	void addition(int a,int b,double c)
+	{
+		System.out.println("result = "+(a+b+c));
+	}
+	void addition(int a,int b,int c,int d)
+	{
+		System.out.println("result = "+(a+b+c+d));
+	}
+	void addition(int a,int b)
+	{
+		System.out.println("result = "+(a+b));
+	}
+    public static void main(String[] args)
+    {
+    	Overloading o=new Overloading();
+	    o.addition(2, 5);//int,int
+	    o.addition(2, 4,1,5);//int,int,int,int
+	    o.addition(1,2,3);//int,int,int
+	    o.addition(2,8, 5.6);//int,int,double
+    }
+}
