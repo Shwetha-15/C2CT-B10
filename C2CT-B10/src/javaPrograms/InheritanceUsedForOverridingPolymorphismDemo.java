ca06b739254b57b50e85ed38472dcd9789c2ba7d
Shwetha-15 <shwetha151998@gmail.com>
2022-02-10 15:00:35 +0530

    3rd commit

---
+++ C2CT-B10/src/javaPrograms/InheritanceUsedForOverridingPolymorphismDemo.java
@@ -0,0 +1,39 @@
+package javaPrograms;
+public class InheritanceUsedForOverridingPolymorphismDemo extends A
+{
+    int a=2;
+    int b=3;
+    int c;
+    void sum()
+    {
+    	c=a+b;
+    	System.out.println("result = "+c);
+    }
+    void multiplication(int a,double d)
+    {
+    	System.out.println("Multiplication result = "+(a*d));
+    	System.out.println("hello child");
+    }
+    void display()
+    {
+    	System.out.println("hello child");
+    }
+    public static void main(String[] args) 
+    {
+    	InheritanceUsedForOverridingPolymorphismDemo i=new InheritanceUsedForOverridingPolymorphismDemo();
+		i.sum();
+		i.multiplication(2, 1.5);
+	}
+}
+class A
+{
+	void multiplication(int a,double d)
+    {
+    	System.out.println("Multiplication main result = "+(a*d));
+    	System.out.println("hello child");
+    }
+	void display()
+    {
+    	System.out.println("hello parent");
+    }
+}
