40c02b30ca14d80ed7ca91d84e208b13daa18fd1
Shwetha-15 <shwetha151998@gmail.com>
2022-02-15 17:16:15 +0530

    commit

--- C2CT-B10/src/day5/AbstractClassDemo.java
+++ C2CT-B10/src/day5/AbstractClassDemo.java
@@ -6,23 +6,23 @@ abstract class Vehicle
 	abstract void m1();
 	void m2()
 	{
-		
+		System.out.println("In m2");
 	}
-	public class Activa extends Vehicle
+	class Activa extends Vehicle
 	{
 		void m1()
 		{
-			
+			System.out.println("In m1");
 		}
 	}
-}
 public class AbstractClassDemo 
 {
-    public static void main(String[] args) 
+    public void main(String[] args) 
     {
 		//Vehicle v=new Vehicle();
     	Activa a=new Activa();
     	a.m1();
     	a.m2();
 	}
+}
 }
