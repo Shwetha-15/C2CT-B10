5b78a2fdccbeb55479265b304b68fbf56107e1d0
Shwetha-15 <shwetha151998@gmail.com>
2022-02-18 17:47:38 +0530

    1st commit

--- C2CT-B10/src/day9/FinalKeyword_Class.java
+++ C2CT-B10/src/day9/FinalKeyword_Class.java
@@ -1,10 +1,34 @@
 package day9;
-
-public class FinalKeyword_Class {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+final class FinalDemo
+{
+	public void m1()
+	{
+		System.out.println("I am Inside m1");
+	}
+}
+class FinalDemo1
+{
+	public void m2()
+	{
+		System.out.println("I am Inside m2");
+	}
+}
+public class FinalKeyword_Class extends FinalDemo1 //we can not extend final class
+{    
+	//we can not extend FinalDemo class
+	/*public final void m1()
+	{
+		System.out.println("Overriding m1");
+	}*/
+	//we can extend FinalDemo1 class
+	public void m2()
+	{
+		System.out.println("Overriding m2");
+	}
+	public static void main(String[] args) 
+	{
+		FinalKeyword_Class f=new FinalKeyword_Class();
+		f.m2();
 	}
 
 }
