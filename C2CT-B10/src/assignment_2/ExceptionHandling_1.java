7e27227a37b596ef31ff75149fdf8d8331011cdc
Shwetha-15 <shwetha151998@gmail.com>
2022-02-20 13:17:55 +0530

    1st commit

---
+++ C2CT-B10/src/assignment_2/ExceptionHandling_1.java
@@ -0,0 +1,20 @@
+package assignment_2;
+import java.util.*;
+public class ExceptionHandling_1 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Main starts");
+		try 
+		{
+			System.out.println(5/0);//Exception occurs in this line because we can not divide 5 by 0	
+		} 
+		catch (Exception e) 
+		{
+			// TODO: handle exception
+			System.out.println("End of program!");
+		}
+		System.out.println("Main ends");
+	}
+
+}
