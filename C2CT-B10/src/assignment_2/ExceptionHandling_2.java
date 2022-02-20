43e127c1c6a2439b1adaaf4e14212a66781c8860
Shwetha-15 <shwetha151998@gmail.com>
2022-02-20 13:23:50 +0530

    2nd commit

---
+++ C2CT-B10/src/assignment_2/ExceptionHandling_2.java
@@ -0,0 +1,22 @@
+package assignment_2;
+public class ExceptionHandling_2
+{	
+	public static void main (String[] args) 
+	{
+		int a=10;
+		System.out.println("Main starts");
+		try 
+		{
+			for(int i=3;i>=0;i--)
+			{
+			    System.out.println(a/i);  
+		    }		
+		} 
+		catch (Exception e) 
+		{
+			// TODO: handle exception
+		}
+		System.out.println("Main ends");
+	}
+
+}
