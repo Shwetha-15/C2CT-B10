b6a1594612160f8cbe2c6a09fcd9d9f83486d36e
Shwetha-15 <shwetha151998@gmail.com>
2022-02-21 20:39:53 +0530

    4th commit

---
+++ C2CT-B10/src/assignment_2/ExceptionDemo.java
@@ -0,0 +1,37 @@
+package assignment_2;
+import java.util.*;
+class Exception
+{
+	static void func(int a,int b)  
+	{ 
+		   int[] arr={1,2,3};
+		   System.out.println(10/a);
+		   System.out.println(arr[b]);
+    }
+}
+public class ExceptionDemo 
+{
+	public static void main (String[] args) throws ArithmeticException,NullPointerException
+	{
+		Scanner in=new Scanner(System.in);
+		System.out.println("Enter the numbers");
+		int a=in.nextInt();
+		int b=in.nextInt();
+		Exception.func(a, b);
+		for(int i=0;i<3;i++)
+		{
+		    try
+		    {
+		    	Exception.func(in.nextInt(),in.nextInt());
+    		}
+		    catch(ArithmeticException e)
+		    {
+    		   System.out.println("can't divide by zero");
+    		}
+		    catch(ArrayIndexOutOfBoundsException e)
+		    {
+    		   System.out.println("Out of bounds!");
+    		}
+		 }
+	}
+}
