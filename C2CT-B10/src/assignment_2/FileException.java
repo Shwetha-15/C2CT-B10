532356883c2a7f325d6a8942278e177e09b0aec3
Shwetha-15 <shwetha151998@gmail.com>
2022-02-21 21:32:45 +0530

    5th commit

---
+++ C2CT-B10/src/assignment_2/FileException.java
@@ -0,0 +1,22 @@
+package assignment_2;
+import java.io.*;
+class FileException 
+{
+	public static void findFile() throws IOException {
+
+	       File newFile = new File("test.txt");
+	    FileInputStream stream = new FileInputStream(newFile);
+	  }
+
+	  public static void main(String[] args) 
+	  {
+		  try 
+		  {
+		      findFile();
+		  }
+		  catch (IOException e) 
+		  {
+		      System.out.println(e);
+		  }
+	  }
+}
