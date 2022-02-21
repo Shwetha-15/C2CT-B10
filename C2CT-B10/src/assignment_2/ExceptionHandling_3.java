bb6fa8cc8d3cd0ce046ac7747edbb30b577eea16
Shwetha-15 <shwetha151998@gmail.com>
2022-02-21 08:54:08 +0530

    3rd commit

--- C2CT-B10/src/assignment_2/ExceptionHandling_3.java
+++ C2CT-B10/src/assignment_2/ExceptionHandling_3.java
@@ -1,27 +1,28 @@
 package assignment_2;
 public class ExceptionHandling_3 
 {
-	   	static void canVote(int age)
+	   	static void canVote(int age) 
 	   	{
 	   	   if(age<18)
 	   	   {
-	           System.out.println("you are not an adult!");
+	   		   try 
+	   		   {
+	   			   throw new Exception();
+	   	       }
+	   		   catch (Exception e) 
+	   		   {
+			       // TODO: handle exception
+	   			   System.out.println("you are not an adult!");
+		       }
 	   	   }
-	       else
+	   	   else
 	       {
 	   		   System.out.println("you can vote!");
 	       }
 	   	}
 	   	public static void main (String[] args) 
 	   	{
-	   		try 
-	   		{
-	   			canVote(20);
-	   			//canVote(10);
-			} 
-	   		catch (Exception e) 
-	   		{
-				// TODO: handle exception
-			}
+	        	canVote(20);
+	   			canVote(10);
 	   	}
 }
