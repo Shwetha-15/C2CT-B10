91c7c33a4327f129e2ab504723bb62d2c8e69e01
Shwetha-15 <shwetha151998@gmail.com>
2022-02-22 21:03:20 +0530

    8th commit

---
+++ C2CT-B10/src/Practice1/Program8.java
@@ -0,0 +1,16 @@
+package Practice1;
+public class Program8 
+{
+    public static void main(String[] args) 
+    {
+    	try
+    	{
+            System.out.println(4/0);                                                                                 
+            
+        }
+    	finally
+        {
+            System.out.println("cleaning.......");
+        }
+	}
+}
