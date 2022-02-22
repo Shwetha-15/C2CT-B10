3910da516e07f95fa6e9b0c6832c30c131d368f1
Shwetha-15 <shwetha151998@gmail.com>
2022-02-22 20:58:44 +0530

    7th commit

---
+++ C2CT-B10/src/Practice1/Program7.java
@@ -0,0 +1,20 @@
+package Practice1;
+public class Program7 
+{
+    public static void main(String[] args) 
+    {
+    	try
+    	{
+            System.out.println(4/0);   
+    	}
+    	catch(Exception e)
+        {
+            System.out.println(e);       
+        }
+        finally
+        {
+            System.out.println("finally executed");
+        }
+    	System.out.println("end of the program");	
+	}
+}
