adb27bc6e0ba2fbc5c74697d19fc2e789b50b4a7
Shwetha-15 <shwetha151998@gmail.com>
2022-02-28 08:32:57 +0530

    4th commit

--- C2CT-B10/src/Practice2/ArrayListDemo2.java
+++ C2CT-B10/src/Practice2/ArrayListDemo2.java
@@ -7,13 +7,15 @@ public class ArrayListDemo2
     	ArrayList a1=new ArrayList();
 		a1.add(10.1);
 		a1.add(18);
+		a1.add("Hello");
 		a1.add(101);
+		a1.add('d');
 		a1.add(10);
 		a1.add(null);
 		a1.add(15);
-		a1.add("Hello");
+		a1.add("DBOSS");
 		a1.add(17);
-		a1.add('A');
+		a1.add('S');
 		a1.add(111.123);
 		System.out.println(a1);
 		
@@ -45,6 +47,36 @@ public class ArrayListDemo2
 				{
 					System.out.println(i2);
 				}
+			}
+		}
+		System.out.println("the String values are");
+		System.out.println("--------------------");
+		for (int i = 0; i < a1.size(); i++) 
+		{
+			Object obj=a1.get(i);
+			if(obj instanceof String)
+			{
+				System.out.println(a1.get(i));
+			}
+		}
+		System.out.println("the Double values are");
+		System.out.println("--------------------");
+		for (int i = 0; i < a1.size(); i++) 
+		{
+			Object d=a1.get(i);
+			if(d instanceof Double)
+			{
+				System.out.println(a1.get(i));
+			}
+		}
+		System.out.println("the Character values are");
+		System.out.println("--------------------");
+		for (int i = 0; i < a1.size(); i++) 
+		{
+			Object c=a1.get(i);
+			if(c instanceof Character)
+			{
+				System.out.println(a1.get(i));
 			}
 		}
 	}
