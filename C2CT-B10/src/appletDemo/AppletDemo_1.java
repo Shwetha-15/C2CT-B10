09142938d7383ef8d16e1eb42c9ad4231ff38c42
Shwetha-15 <shwetha151998@gmail.com>
2022-03-23 13:32:42 +0530

    1st commit

---
+++ C2CT-B10/src/appletDemo/AppletDemo_1.java
@@ -0,0 +1,10 @@
+package appletDemo;
+import java.applet.Applet;
+import java.awt.Graphics;
+public class AppletDemo_1 extends Applet
+{
+    public void paint(Graphics g)
+    {
+    	g.drawString("DBOSS",50,50);
+    }
+}
